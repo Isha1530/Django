794f510a5f3ea7c0dcea3ab2919a3018b404d88a
Isha1530 <147532644+Isha1530@users.noreply.github.com>
2025-05-22 08:09:14 +0530

    Delete may13 directory

--- may13/appp/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Category
-# Register your models here.
-
-class CategoryAdmin(admin.ModelAdmin):
-    list_display = ['id', 'name', 'image']
-
-admin.site.register(Category, CategoryAdmin)
--- may13/appp/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class Category(models.Model):
-    name = models.CharField(max_length=50)
-    image = models.ImageField(upload_to='category_images/')
